951c29d3f238630bdd6738e715bf5da640a127eb
Amy-RT <95489289+Amy-RT@users.noreply.github.com>
2025-07-23 22:40:29 +0100

    Implement radio button functionality

--- app.py
+++ app.py
@@ -3,21 +3,27 @@
 
 
 from dash import Dash, html, dcc
+from dash.dependencies import Input, Output
 import plotly.express as px
 import pandas as pd
 
 app = Dash()
 
 DATA_PATH = './data/prepared_pink_morsel_data.csv'
-df = pd.read_csv(DATA_PATH)
-df = df.sort_values(by="date")
+pink_morsel_df = pd.read_csv(DATA_PATH)
+pink_morsel_df = pink_morsel_df.sort_values(by="date")
 
 fig = px.line(
-    df,
+    pink_morsel_df,
     x='date',         
     y='total_sales',  
     title='Daily Pink Morsel sales for the period 2018-2022:' 
 )
+fig.update_layout(
+        xaxis_title="Date",
+        yaxis_title="Total Sales ($)",
+        hovermode="x unified" # Ensures consistent hover info across lines if multiple
+    )
 
 app.layout = html.Div(children=[
     html.H1(children=' Pink Morsel: Cross  Regional Sales History',
@@ -51,15 +57,44 @@ app.layout = html.Div(children=[
         ],
         id='region-radio-container'
     ),
-
-    # html.Div(children='''
-    #     Dash: A web application framework for your data.
-    # '''),
-
-    
-
-    
 ])
 
+# --- Callback to connect radio functions to graph ---
+@app.callback(
+    Output('visualisation', 'figure'),
+    Input('region-radio-buttons', 'value')
+)
+def update_graph(selected_region):
+    # work with a copy of the original dataframe
+    df_filtered_by_region = pink_morsel_df.copy()
+
+    chart_title = ""
+
+    if selected_region == 'all':
+        df_for_chart = df_filtered_by_region
+        chart_title = 'Pink Morsel Sales Across All Regions'
+    else:
+        # Filter by region
+        df_for_chart = df_filtered_by_region[df_filtered_by_region['region'] == selected_region]
+        chart_title = f'Pink Morsel Sales in the {selected_region.title()} Region'
+
+    # Create the line chart
+    fig = px.line(
+        df_for_chart,
+        x='date',
+        y='total_sales',
+        title=chart_title
+    )
+
+    fig.update_layout(
+        xaxis_title="Date",
+        yaxis_title="Total Sales ($)",
+        hovermode="x unified" # Ensures consistent hover info across lines if multiple
+    )
+
+    return fig
+
+
+# --- Run the app ---
 if __name__ == '__main__':
     app.run(debug=True)
